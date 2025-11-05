b3d52d9f0fac5c0395939d308a840e8655982b24
Billinwg <billinwgkay@gmail.com>
2025-11-04 19:47:55 -0500

    substract and divide tests

--- src/test/java/edu/unac/CalculatorTest.java
+++ src/test/java/edu/unac/CalculatorTest.java
@@ -16,4 +16,16 @@ class CalculatorTest {
         Calculator calculator = new Calculator();
         assertEquals(6, calculator.multiply(2, 3));
     }
+
+    @Test
+    void testSubtraction(){
+        Calculator calculator = new Calculator();
+        assertEquals(1, calculator.sub(2, 1));
+    }
+
+    @Test
+    void testDivision(){
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.div(4, 2));
+    }
 }
